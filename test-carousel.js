0173519fc390ff73db85d7381a825009ebce88ce
chise <touyu.love1205@gmail.com>
2017-12-26 10:25:10 +0000

    site

---
+++ test-carousel.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel(
+        {
+          loop: true,
+          nav: false,
+          dots: false,
+          items: 1,
+          autoplay: true 
+        }
+    );
+});
